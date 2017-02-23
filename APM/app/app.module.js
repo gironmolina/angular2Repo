8c5ed2affc1ff9c13ec5bf4de36433c5f16e498a
Huans Hirons <gironmolina@gmail.com>
2017-02-23 02:19:10 +0200

    Added routed pages

--- APM/app/app.module.js
+++ APM/app/app.module.js
@@ -13,6 +13,9 @@ var app_component_1 = require("./app.component");
 var product_list_component_1 = require("./products/product-list.component");
 var product_filter_pipe_1 = require("./products/product-filter.pipe");
 var star_component_1 = require("./shared/star.component");
+var welcome_component_1 = require("./home/welcome.component");
+var product_detail_component_1 = require("./products/product-detail.component");
+var router_1 = require("@angular/router");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -23,11 +26,20 @@ AppModule = __decorate([
         imports: [
             platform_browser_1.BrowserModule,
             forms_1.FormsModule,
-            http_1.HttpModule
+            http_1.HttpModule,
+            router_1.RouterModule.forRoot([
+                { path: "products", component: product_list_component_1.ProductListComponent },
+                { path: "product/:id", component: product_detail_component_1.ProductDetailComponent },
+                { path: "welcome", component: welcome_component_1.WelcomeComponent },
+                { path: "", redirectTo: "welcome", pathMatch: "full" },
+                { path: "**", redirectTo: "welcome", pathMatch: "full" }
+            ])
         ],
         declarations: [
             app_component_1.AppComponent,
+            welcome_component_1.WelcomeComponent,
             product_list_component_1.ProductListComponent,
+            product_detail_component_1.ProductDetailComponent,
             product_filter_pipe_1.ProductFilterPipe,
             star_component_1.StarComponent
         ],
--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -3,19 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent }  from './app.component';
-import { ProductListComponent }  from './products/product-list.component';
+import { ProductListComponent } from './products/product-list.component';
 import { ProductFilterPipe }  from './products/product-filter.pipe';
 import { StarComponent }  from './shared/star.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
-    HttpModule 
+    HttpModule ,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "product/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" }
+    ])
     ],    
   declarations: [ 
     AppComponent, 
+    WelcomeComponent,
     ProductListComponent,
+    ProductDetailComponent,
     ProductFilterPipe,
     StarComponent
     ],
